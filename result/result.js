6ee3a5266563ce897991ab3de332639e74fb3931
Abdullahi Salaudeen <abdullahisalaudeen65@gmail.com>
2025-07-05 13:13:09 +0100

    update score

--- result/result.js
+++ result/result.js
@@ -1,32 +1,32 @@
 // import { score } from "../qustion/question.js";
 //make the name display
-const getName = localStorage.getItem('name');
-const score = JSON.parse(localStorage.getItem('score')); 
-const question = JSON.parse(localStorage.getItem('quizQuestions')); 
-console.log(question); 
+const getName = localStorage.getItem("name");
+const score = JSON.parse(localStorage.getItem("score"));
+const question = JSON.parse(localStorage.getItem("quizQuestions"));
+console.log(question);
 
-//display name 
-const perName = document.getElementById('person-name');
-perName.textContent = getName; 
+//display name
+const perName = document.getElementById("person-name");
+perName.textContent = getName;
 
-const total = document.getElementById('score');
-if (score >= 5) {
-    total.innerHTML = `Congratulations !!!, <span id="person-name">${getName}</span><br>
+const total = document.getElementById("score");
+if (score >= question.length / 2) {
+  total.innerHTML = `Congratulations !!!, <span id="person-name">${getName}</span><br>
     You PASSED with total mark of <br>
-    <span>${score}/${question.length}</span>`
-}else if(!score){
-    total.innerHTML = `Better luck next time !!!, <span id="person-name">${getName}</span><br>
+    <span>${score}/${question.length}</span>`;
+} else if (!score) {
+  total.innerHTML = `Better luck next time !!!, <span id="person-name">${getName}</span><br>
     You Failed with a total mark of <br>
-    <span>0/${question.length}</span>`
-}else{
-    total.innerHTML = `Better luck next time !!!, <span id="person-name">${getName}</span><br>
+    <span>0/${question.length}</span>`;
+} else {
+  total.innerHTML = `Better luck next time !!!, <span id="person-name">${getName}</span><br>
     You Failed with a total mark of <br>
-    <span>${score}/${question.length}</span>`
+    <span>${score}/${question.length}</span>`;
 }
 
 //remove score from local storage
-const home = document.querySelector('.Go-home');
-home.addEventListener('click', ()=>{
-    localStorage.removeItem('score');
-    localStorage.removeItem("name");
-})
+const home = document.querySelector(".Go-home");
+home.addEventListener("click", () => {
+  localStorage.removeItem("score");
+  localStorage.removeItem("name");
+});
